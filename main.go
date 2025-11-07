26d15ec845f51c984b5b5295da5114d28c0beff1
aaronb12-ux <afairster03@gmail.com>
2025-11-06 21:21:01 -0800

    added some logic on starting the quiz. need to implement timer logic

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log" //errors
 	"os" //opening the file
 
+
+
 )
 
 func openFile() [][]string {
@@ -24,7 +26,6 @@ func openFile() [][]string {
 	records, err := csvReader.ReadAll() //reading all of the contents
 										//This stores each line into an array 
 										//Each line is its own element where each comma separated value is an index
-
 	if err != nil { //fails when there is no comma on line
 		log.Fatal("Error reading file contents")
 	}
@@ -41,7 +42,9 @@ func getQandA(file_data [][]string ) ([]string, []string) { //creating separate 
 
 	for i := 0; i < len(file_data); i++ {
 		questions = append(questions, file_data[i][0]) 
+
 		answers = append(answers, file_data[i][1])
+	
 	}
 
 	return questions, answers
@@ -52,14 +55,13 @@ func takeQuiz(questions []string, answers []string) int {
 	//display the question
 	//scan in an answer
 	//check if the answer is equal to the
+
 	var answer string
 	var numCorrect int
 
 	for i := 0; i < len(questions); i ++ {
 		fmt.Print(questions[i] + " ")
 		fmt.Scanf("%s", &answer)
-
-		
 
 		if answer == answers[i] {
 			numCorrect = numCorrect + 1
@@ -79,15 +81,22 @@ func showResults(numberCorrect int, numberQuestions int) {
 
 func main() {
 
+	var begin string
+
 	file_data := openFile()
 
-	questions, answers := getQandA(file_data)
+	fmt.Print("You have 5 seconds to finish the quiz. Type 'start' and press enter to begin the quiz ")
+	fmt.Scanf("%s", &begin)
+
+	if begin == "start" {
+		questions, answers := getQandA(file_data)
 
 	num_correct := takeQuiz(questions, answers)
 
+	fmt.Println("Time ran out")
+
 	showResults(num_correct, len(answers))
-
-
+	}
 }
 
 
